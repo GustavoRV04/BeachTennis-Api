a065d449ec58261319593ace8b0ac04443381e8d
GUSTAVO DA ROCHA VIANA <02856027059@senacrs.edu.br>
2026-09-25 20:18:47 -0300

    UseCases

--- BeachTennis.Application/Exceptions/ReservaConflitanteException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BeachTennis.Application.Exceptions;
-
-public class ReservaConflitanteException : Exception
-{
-    public ReservaConflitanteException()
-        : base("Já existe uma reserva confirmada para esta quadra neste horário.") { }
-}
